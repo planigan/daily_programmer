eeb1bad2e3076c77996ea6b578fa08c4aee79046
J. Patrick Lanigan <planigan@gmail.com>
2017-07-20 13:03:47 -0400

    Clean up 314

--- solutions/concat_int_314_easy.js
+++ solutions/concat_int_314_easy.js
@@ -1,10 +1,8 @@
-const sortIntArray = arr => {
-  return arr.sort((a,b) => {
-    return parseInt(a+b) - parseInt(b+a)
-  })
-}
+const sortIntArray = arr => arr.sort((a,b) => parseInt(a+b) - parseInt(b+a))
 
 const getMinConcatInt = arr => sortIntArray(arr).join('')
+
 const getMaxConcatInt = arr => sortIntArray(arr).reverse().join('')
+
 exports.getOutput =
   arr => `${getMinConcatInt(arr.split(' '))} ${getMaxConcatInt(arr.split(' '))}`
